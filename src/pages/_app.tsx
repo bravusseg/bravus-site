97525b622987e15110bca20f07d40a84335857fc
William Américo <william.americo@hotmail.com>
2025-04-30 09:58:50 -0300

    fix: carregar Tailwind via CDN diretamente em _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+// src/pages/_app.tsx
 import '../styles/globals.css';
++ import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
++     <Head>
++       <script src="https://cdn.tailwindcss.com"></script>
++     </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
